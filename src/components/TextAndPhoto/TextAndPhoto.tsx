495525a74ed0d46ccd3675698073c2779d2ac250
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-08 22:15:23 +0100

    possible fix5

--- src/components/TextAndPhoto/TextAndPhoto.tsx
+++ src/components/TextAndPhoto/TextAndPhoto.tsx
@@ -5,11 +5,6 @@ import { Container } from "react-bootstrap"
 import Icon from "../../images/Navbar-Icon.png"
 
 
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
-
-AOS.init();
-
 const TextAndPhoto = () => {
     return(
         <section className={TextAndPhotoStyle.TextAndPhoto}>
@@ -20,10 +15,10 @@ const TextAndPhoto = () => {
                 <div className={TextAndPhotoStyle.TypeWriterName}>
                     <h1 className={TextAndPhotoStyle.TypeWriterNameText}>Hello my name is Pietro!</h1>
                 </div>
-                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.DescriptionContainer}>
+                <div className={TextAndPhotoStyle.DescriptionContainer}>
                     <span className={TextAndPhotoStyle.Description}>I'm Pietro Costanzi Fantini, a Frontend developer and Graphics designer. I'm a young, dynamic and resourceful teen open to any kind of life/working experience and eager to prove his worth!</span>
                 </div>
-                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.IconContainer}>
+                <div className={TextAndPhotoStyle.IconContainer}>
                     <img className={TextAndPhotoStyle.Icon} src={Icon} alt=""/>
                 </div>
             </Container>
