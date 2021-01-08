2958e4f9a5b6396265315318212cc492a8921b18
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-08 22:25:54 +0100

    possible fix6

--- src/components/TextAndPhoto/TextAndPhoto.tsx
+++ src/components/TextAndPhoto/TextAndPhoto.tsx
@@ -4,7 +4,9 @@ import ProfilePic from "../../images/ProfilePic.png"
 import { Container } from "react-bootstrap"
 import Icon from "../../images/Navbar-Icon.png"
 
-import AOS from "aos"
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 AOS.init();
 
 const TextAndPhoto = () => {
@@ -16,6 +18,7 @@ const TextAndPhoto = () => {
                 </div>
                 <div className={TextAndPhotoStyle.TypeWriterName}>
                     <h1 className={TextAndPhotoStyle.TypeWriterNameText}>Hello my name is Pietro!</h1>
+                </div>
                 <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.DescriptionContainer}>
                     <span className={TextAndPhotoStyle.Description}>I'm Pietro Costanzi Fantini, a Frontend developer and Graphics designer. I'm a young, dynamic and resourceful teen open to any kind of life/working experience and eager to prove his worth!</span>
                 </div>
