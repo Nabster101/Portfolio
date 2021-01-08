fc21eea57143613feebb34f35a64be40a384a433
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-08 21:24:30 +0100

    Changed background color and added some animations

--- src/components/TextAndPhoto/TextAndPhoto.tsx
+++ src/components/TextAndPhoto/TextAndPhoto.tsx
@@ -4,8 +4,15 @@ import ProfilePic from "../../images/ProfilePic.png"
 import { Container } from "react-bootstrap"
 import Icon from "../../images/Navbar-Icon.png"
 
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+AOS.init();
+
 const TextAndPhoto = () => {
     return(
+        
         <section className={TextAndPhotoStyle.TextAndPhoto}>
             <Container className={TextAndPhotoStyle.Container}>
                 <div className={TextAndPhotoStyle.ProfilePicContainer}>
@@ -14,14 +21,13 @@ const TextAndPhoto = () => {
                 <div className={TextAndPhotoStyle.TypeWriterName}>
                     <h1 className={TextAndPhotoStyle.TypeWriterNameText} >Hello my name is Pietro!</h1>
                 </div>
-                <div className={TextAndPhotoStyle.DescriptionContainer}>
+                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.DescriptionContainer}>
                     <span className={TextAndPhotoStyle.Description}>I'm Pietro Costanzi Fantini, a Frontend developer and Graphics designer. I'm a young, dynamic and resourceful teen open to any kind of life/working experience and eager to prove his worth!</span>
                 </div>
-                <div className={TextAndPhotoStyle.IconContainer}>
+                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true"  className={TextAndPhotoStyle.IconContainer}>
                     <img className={TextAndPhotoStyle.Icon} src={Icon} alt=""/>
                 </div>
             </Container>
-            
         </section>
     )
 }
