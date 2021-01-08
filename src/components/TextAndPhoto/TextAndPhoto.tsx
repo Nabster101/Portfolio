2ab26f2742a59a1efa33a1d0239b71b6411ae5b6
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-08 15:08:55 +0100

    Starting Text and photo section

--- src/components/TextAndPhoto/TextAndPhoto.tsx
+++ src/components/TextAndPhoto/TextAndPhoto.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import TextAndPhotoStyle from "./TextAndPhoto.module.scss"
+import ProfilePic from "../../images/ProfilePic.png"
 
 const TextAndPhoto = () => {
     return(
         <section className={TextAndPhotoStyle.TextAndPhoto}>
-            
+            <div className={TextAndPhotoStyle.ProfilePic}>
+                <img src={ProfilePic} alt=""/>
+            </div>
         </section>
     )
 }
