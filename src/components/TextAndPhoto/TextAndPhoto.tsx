f7f57b84395c10a5729d6c1385d37d8146ed76cd
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-08 21:34:31 +0100

    possible fix

--- src/components/TextAndPhoto/TextAndPhoto.tsx
+++ src/components/TextAndPhoto/TextAndPhoto.tsx
@@ -6,25 +6,24 @@ import Icon from "../../images/Navbar-Icon.png"
 
 
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import 'aos/dist/aos.css'; 
 
 AOS.init();
 
 const TextAndPhoto = () => {
     return(
-        
         <section className={TextAndPhotoStyle.TextAndPhoto}>
             <Container className={TextAndPhotoStyle.Container}>
                 <div className={TextAndPhotoStyle.ProfilePicContainer}>
                     <img className={TextAndPhotoStyle.ProfilePic} src={ProfilePic} alt=""/>
                 </div>
                 <div className={TextAndPhotoStyle.TypeWriterName}>
-                    <h1 className={TextAndPhotoStyle.TypeWriterNameText} >Hello my name is Pietro!</h1>
+                    <h1 className={TextAndPhotoStyle.TypeWriterNameText}>Hello my name is Pietro!</h1>
                 </div>
                 <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.DescriptionContainer}>
                     <span className={TextAndPhotoStyle.Description}>I'm Pietro Costanzi Fantini, a Frontend developer and Graphics designer. I'm a young, dynamic and resourceful teen open to any kind of life/working experience and eager to prove his worth!</span>
                 </div>
-                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true"  className={TextAndPhotoStyle.IconContainer}>
+                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.IconContainer}>
                     <img className={TextAndPhotoStyle.Icon} src={Icon} alt=""/>
                 </div>
             </Container>
