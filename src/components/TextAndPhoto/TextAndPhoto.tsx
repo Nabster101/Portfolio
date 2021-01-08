ca9756cf7f8fb971a6b20fa8b275504e708aff20
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-09 00:00:26 +0100

    Section updated

--- src/components/TextAndPhoto/TextAndPhoto.tsx
+++ src/components/TextAndPhoto/TextAndPhoto.tsx
@@ -19,14 +19,13 @@ const TextAndPhoto = () => {
                 <div className={TextAndPhotoStyle.TypeWriterName}>
                     <h1 className={TextAndPhotoStyle.TypeWriterNameText} >Hello my name is Pietro!</h1>
                 </div>
-                <div data-aos="zoom-in" data-aos-delay="1600" className={TextAndPhotoStyle.DescriptionContainer}>
+                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="false" className={TextAndPhotoStyle.DescriptionContainer}>
                     <span className={TextAndPhotoStyle.Description}>I'm Pietro Costanzi Fantini, a Frontend developer and Graphics designer. I'm a young, dynamic and resourceful teen open to any kind of life/working experience and eager to prove his worth!</span>
                 </div>
-                <div data-aos="zoom-in" data-aos-delay="1600" className={TextAndPhotoStyle.IconContainer}>
+                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="false" className={TextAndPhotoStyle.IconContainer}>
                     <img className={TextAndPhotoStyle.Icon} src={Icon} alt=""/>
                 </div>
             </Container>
-            
         </section>
     )
 }
