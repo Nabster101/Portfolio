22a8bf81b47b89c065016c73c63e0e5f0271495b
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-08 22:20:19 +0100

    possible fix6

--- src/components/TextAndPhoto/TextAndPhoto.tsx
+++ src/components/TextAndPhoto/TextAndPhoto.tsx
@@ -4,6 +4,8 @@ import ProfilePic from "../../images/ProfilePic.png"
 import { Container } from "react-bootstrap"
 import Icon from "../../images/Navbar-Icon.png"
 
+import AOS from "aos"
+AOS.init();
 
 const TextAndPhoto = () => {
     return(
@@ -14,11 +16,10 @@ const TextAndPhoto = () => {
                 </div>
                 <div className={TextAndPhotoStyle.TypeWriterName}>
                     <h1 className={TextAndPhotoStyle.TypeWriterNameText}>Hello my name is Pietro!</h1>
-                </div>
-                <div className={TextAndPhotoStyle.DescriptionContainer}>
+                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.DescriptionContainer}>
                     <span className={TextAndPhotoStyle.Description}>I'm Pietro Costanzi Fantini, a Frontend developer and Graphics designer. I'm a young, dynamic and resourceful teen open to any kind of life/working experience and eager to prove his worth!</span>
                 </div>
-                <div className={TextAndPhotoStyle.IconContainer}>
+                <div data-aos="zoom-in" data-aos-delay="1600" data-aos-once="true" className={TextAndPhotoStyle.IconContainer}>
                     <img className={TextAndPhotoStyle.Icon} src={Icon} alt=""/>
                 </div>
             </Container>
