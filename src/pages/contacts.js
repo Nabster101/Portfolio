aa77f72606e26c2ff09fd5cd81313cb680eb8871
Nabster101 <pietrocostanzi39@gmail.com>
2021-05-05 13:04:52 +0200

    general fix

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Helmet from "react-helmet"
 import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from "../components/Navbar/Navbar"
 import ContactForm from "../components/ContactForm/ContactForm"
@@ -7,6 +8,10 @@ import ContactFooter from "../components/ContactFooter/ContactFooter"
 export default function Home() {
   return (
     <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pietro Costanzi Fantini</title>
+      </Helmet>
       <div>
           <Navbar/>
           <ContactForm/>
