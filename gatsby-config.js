0987a2829165176f055d0636c5676aa2f69aaf83
Nabster101 <pietrocostanzi39@gmail.com>
2021-05-05 12:39:04 +0200

    Title fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,6 @@ module.exports = {
 			},
 		},
 		`gatsby-plugin-typescript`,
+		`gatsby-plugin-react-helmet`,
 	],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,15 @@ import Presentation from "../components/Presentation/Presentation"
 import Projects from "../components/Projects/Projects"
 import Resume from "../components/Resume/Resume"
 import Footer from "../components/Footer/Footer"
+import Helmet from "react-helmet"
 
 export default function Home() {
   return (
     <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pietro Costanzi Fantini</title>
+      </Helmet>
       <div>
           <Navbar/>
           <TextAndPhoto id="Home"/>
